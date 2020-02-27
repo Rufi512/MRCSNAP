91488a02f1a12254f646a06f34d3965862c0e49d
Rufi512 <Rafajoslk412@hotmail.com>
2020-02-27 08:01:40 -0430

    commits

--- js/changerimg.js
+++ js/changerimg.js
@@ -1,73 +1,70 @@
-var imagenes= new Array();
-imagenes[0]="src/2.jpg";
-imagenes[1]="src/4.jpg";
-imagenes[2]="src/6.jpg";
-imagenes[3]="src/8.jpg";
+var imagenes = new Array();
+imagenes[0] = "src/2.jpg";
+imagenes[1] = "src/4.jpg";
+imagenes[2] = "src/6.jpg";
+imagenes[3] = "src/8.jpg";
 
 
 
-var imagenes2= new Array();
-imagenes2[0]="src/1.jpg";
-imagenes2[1]="src/3.jpg";
-imagenes2[2]="src/5.jpg";
-imagenes2[3]="src/7.jpg";
+var imagenes2 = new Array();
+imagenes2[0] = "src/1.jpg";
+imagenes2[1] = "src/3.jpg";
+imagenes2[2] = "src/5.jpg";
+imagenes2[3] = "src/7.jpg";
 
-var imagenesdev=new Array();
-imagenesdev[0]="src/mobile/1d.jpg";
-imagenesdev[1]="src/mobile/2d.jpg";
-imagenesdev[2]="src/mobile/3d.jpg";
-imagenesdev[3]="src/mobile/4d.jpg";
-imagenesdev[4]="src/mobile/5d.jpg";
-imagenesdev[5]="src/mobile/6d.jpg";
+var imagenesdev = new Array();
+imagenesdev[0] = "src/mobile/1d.jpg";
+imagenesdev[1] = "src/mobile/2d.jpg";
+imagenesdev[2] = "src/mobile/3d.jpg";
+imagenesdev[3] = "src/mobile/4d.jpg";
+imagenesdev[4] = "src/mobile/5d.jpg";
+imagenesdev[5] = "src/mobile/6d.jpg";
 
 
 
-var i=0;
-var ancho = window.innerWidth;
+var i = 0; 
+var ancho = window.innerWidth; //Se almacena el ancho de la pantalla
 
-const pc=()=>{
-   i=Math.round(Math.random()*3);
-  setTimeout(cambiarimg,0);
-  setInterval(cambiarimg,10000);
+const pc = () => {
+    i = Math.round(Math.random() * 3);
+    setTimeout(cambiarimg, 0);
+    setInterval(cambiarimg, 10000);
 };
- cambiarimg=()=>{
-  if (i>3) {
-    i=0;
-  }
-  else{}
-  var changer=document.getElementById("change1");
-  var changer2=document.getElementById("change2");
-  changer.src=imagenes[i];
-  changer2.src=imagenes2[i];
-  i++;
-
-
-};
-
-const device=()=>{
-   i=Math.round(Math.random()*5);
-  setTimeout(imgsdevices,0);
-  setInterval(imgsdevices,10000);
-};
-
-const imgsdevices=()=>{
-var deviceback=document.getElementById("deviceback");
-deviceback.style.backgroundImage="url("+imagenesdev[i]+")";
-
-  if (i>=5) {
-    i=0;
-
-  }
-  else{
+cambiarimg = () => {
+    if (i > 3) {
+        i = 0;
+    } else {}
+    var changer = document.getElementById("change1");
+    var changer2 = document.getElementById("change2");
+    changer.src = imagenes[i];
+    changer2.src = imagenes2[i];
     i++;
-  }
+
+
+};
+
+const device = () => {
+    i = Math.round(Math.random() * 5);
+    setTimeout(imgsdevices, 0);
+    setInterval(imgsdevices, 10000);
+};
+
+const imgsdevices = () => {
+    var deviceback = document.getElementById("deviceback");
+    deviceback.style.backgroundImage = "url(" + imagenesdev[i] + ")";
+
+    if (i >= 5) {
+        i = 0;
+
+    } else {
+        i++;
+    }
 
 };
 
 
-if(ancho>790){
-pc();
-}
-else{
-  device();
+if (ancho > 790) { //Dependiendo del ancho de las pantallas se ejecutara x funcion
+    pc();
+} else {
+    device();
 }
